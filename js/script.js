b60feb435d37b8b0b76f7ed3706f510ec60e2692
federico_viganò <fede_viga@hotmail.it>
2024-02-14 15:51:24 +0100

    milestone 1 da fixare

--- js/script.js
+++ js/script.js
@@ -1,32 +1,84 @@
 /*
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
+Dato un array di oggetti letterali con:
+ - url dell’immagine
+ - titolo
+ - descrizione
+Creare un carosello come nella foto allegata.
+Milestone 0:
+Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+Milestone 1:
+Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+Al click dell'utente sulle frecce verso sinistra o destra, la nuova immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+Milestone 2:
+Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+BONUS 1:
+Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+BONUS 2:
+Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva in automatico.
+BONUS 3:
+Aggiungere bottoni di start / stop e di inversione del meccanismo di autoplay.
 */
 
 
-// bersagliamo lo slider
-// tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
-// per ognuno di essi andremo a creare un elemento img dentro lo slider
 
-const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"];
-console.log(images);
+const images = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
 
 // bersagliamo lo slider
 const sliderElement = document.getElementById("slider");
 
+// creo forEach per visualizzare in pagina le immagini con titolo e descrizione
+images.forEach(element => {
 
-// tramite un ciclo for prendiamo ogni indirizzo delle immagini dall'array
-for (let i = 0; i < images.length; i++) {
+    const carouselElement = document.createElement("div");
+    carouselElement.className = ("slider-element");
+    sliderElement.append(carouselElement);
 
-    sliderElement.innerHTML += `<img src="./img/0${i + 1}.webp" alt="immagine ${i + 1}">`;
+    // per ogni ogetto dell'array aggiungo l'immagine, il titolo e la descrizione
+    for (let key in element) {
 
-    // per ognuno di essi andremo a creare un elemento img dentro lo slider
+        if (key == "image") {
 
-}
+            const carouselImageElement = document.createElement("img");
+            carouselImageElement.src = "./" + element.image;
+            carouselElement.append(carouselImageElement);
 
-document.querySelector("#slider img:nth-of-type(1)").className = "active";
+        } else {
+
+            const carouselDetailElement = document.createElement("div");
+            carouselDetailElement.innerText = element[key];
+            carouselDetailElement.className = key;
+            carouselElement.append(carouselDetailElement);
+
+        }
+    }
+
+});
+
+// rendo attiva la prima immagine
+document.querySelector("#slider .slider-element:nth-of-type(1)").classList.add("active");
 
 /*
 -  salvo un contatore della slide
@@ -47,26 +99,26 @@ document.querySelector("#up-arrow").addEventListener("click", function() {
     if (slideNumber < images.length) {
 
         // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.remove("active");
 
         // - aumento il contatore di 1
         slideNumber++;
 
         // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.add("active");
 
         console.log(slideNumber);
 
     } else {
 
         // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.remove("active");
 
         // resetto la variabile che mi conta l'immagine a cui sono arrivato
         slideNumber = 1;
 
         // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.add("active");
 
     }
 
@@ -78,26 +130,26 @@ document.querySelector("#down-arrow").addEventListener("click", function() {
 
     if (slideNumber > 1) {
         // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.remove("active");
 
         // - diminuisco il contatore di 1
         slideNumber--;
 
         // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.add("active");
 
         console.log(slideNumber);
 
     } else {
 
         // - prendo l'immagine attuale e le rimuovo la classe "active"  
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.remove("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.remove("active");
 
         // - metto il valore di slideNumebr = alla posizione dell'ultima immagine
         slideNumber = images.length;
 
         // - prendo l'immagine con il nuovo contatore e le aggiungo la classe "active"
-        document.querySelector(`#slider img:nth-of-type(${slideNumber})`).classList.add("active");
+        document.querySelector(`#slider .slider-element:nth-of-type(${slideNumber})`).classList.add("active");
 
     }
     
